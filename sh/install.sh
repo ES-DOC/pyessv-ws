ec2165b0910f37e5cec10425ba2880e430151b14
momipsl <momipsl@ipsl.fr>
2019-07-22 20:33:32 +0200

    1. Externalised pip2.

--- sh/install.sh
+++ sh/install.sh
@@ -6,10 +6,6 @@ source $PYESSV_WS_HOME/sh/utils.sh
 # Main entry point.
 main()
 {
-	echo $PYESSV_WS_HOME
-	echo $PYESSV_WS_PIP
-	echo $PYESSV_WS_HOME/resources/requirements.txt
-
 	_install_ops_dir
 	_install_config
 	_install_venv
@@ -35,15 +31,15 @@ _install_venv()
 {
     log "installing virtual environment ..."
 
-    # ... install venv.
-    $PYESSV_WS_PIP install --upgrade pip
-    $PYESSV_WS_PIP install --upgrade virtualenv
+    # ... install venv using system pip.
+    pip2 install --upgrade pip
+    pip2 install --upgrade virtualenv
     virtualenv $PYESSV_WS_HOME/ops/venv
 
     # ... install dependencies.
     source $PYESSV_WS_HOME/sh/activate_venv.sh
-    pip install --upgrade pip
-    pip install --upgrade --no-cache-dir -I -r $PYESSV_WS_HOME/resources/requirements.txt
+    pip2 install --upgrade pip
+    pip2 install --upgrade --no-cache-dir -I -r $PYESSV_WS_HOME/resources/requirements.txt
 
     deactivate
 	log "virtual environment installed"
